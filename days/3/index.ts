24ed9e7dde0ff1967a9df93a9126d5340295d025
sire-sam <sire-sam@users.noreply.github.com>
2022-12-03 22:47:42 +0100

    Day 3 - part 1 - test

---
+++ days/3/index.ts
@@ -0,0 +1,34 @@
+import { RegExp } from "https://cdn.esm.sh/v78/webidl-conversions@3.0.1/es2021/webidl-conversions.js";
+
+const textDecoder = new TextDecoder("utf-8");
+const dirPath = new URL(".", import.meta.url).pathname;
+
+const chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const expectedTestResult = 157;
+
+const splitLineHalf = (l: string): [string, string] => [l.slice(0, l.length / 2), l.slice(l.length /2, l.length)];
+const charMatchInLines = ([l1, l2]: [string, string]) => l2[l2.search(new RegExp(`[${l1}]`))];
+const charWeight = (char: string) => chars.indexOf(char) + 1;
+const sum = (a, b) => a + b;
+const logEntry = (e) => console.log(e);
+
+Deno.readFile(dirPath + "./test.txt").then(
+  (res) => {
+    return textDecoder.decode(res)
+      .split(/\n/)
+      .map(splitLineHalf)
+      .map(charMatchInLines)
+      .map(charWeight)
+      .reduce(sum, 0)
+  }
+).then((r) => {
+  console.assert(r === expectedTestResult, r, `should be ${expectedTestResult}`);
+  console.log(r);
+});
+
+// Split by line
+//   for each
+//      split Hald (length/2) ?
+//      find letter in both string
+//      get weight of letter
+//      sum + weight
