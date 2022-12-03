ecf561b0716d48f5ab6607b79114aaff8366287a
sire-sam <sire-sam@users.noreply.github.com>
2022-12-03 22:50:38 +0100

    Day 3 - part 1 - run input

--- days/3/index.ts
+++ days/3/index.ts
@@ -3,28 +3,35 @@ import { RegExp } from "https://cdn.esm.sh/v78/webidl-conversions@3.0.1/es2021/w
 const textDecoder = new TextDecoder("utf-8");
 const dirPath = new URL(".", import.meta.url).pathname;
 
+const isRunningTest = false;
+const testInputPath = dirPath + "./test.txt";
+const inputPath = dirPath + "./input.txt";
+
 const chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
 const expectedTestResult = 157;
 
-const splitLineHalf = (l: string): [string, string] => [l.slice(0, l.length / 2), l.slice(l.length /2, l.length)];
+const splitLineHalf = (l: string): [string, string] => [l.slice(0, l.length / 2), l.slice(l.length / 2, l.length)];
 const charMatchInLines = ([l1, l2]: [string, string]) => l2[l2.search(new RegExp(`[${l1}]`))];
 const charWeight = (char: string) => chars.indexOf(char) + 1;
 const sum = (a, b) => a + b;
 const logEntry = (e) => console.log(e);
 
-Deno.readFile(dirPath + "./test.txt").then(
-  (res) => {
-    return textDecoder.decode(res)
+Deno.readFile(isRunningTest ? testInputPath : inputPath)
+  .then(
+    (res) => textDecoder.decode(res)
       .split(/\n/)
       .map(splitLineHalf)
       .map(charMatchInLines)
       .map(charWeight)
       .reduce(sum, 0)
-  }
-).then((r) => {
-  console.assert(r === expectedTestResult, r, `should be ${expectedTestResult}`);
-  console.log(r);
-});
+  )
+  .then((r) => {
+      if (isRunningTest) {
+        console.assert(r === expectedTestResult, r, `should be ${expectedTestResult}`);
+      }
+      console.log(r);
+    }
+  );
 
 // Split by line
 //   for each
