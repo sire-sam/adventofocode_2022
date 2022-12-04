fe2dc1d61daa3919e3a78503a6433841ce01f0d9
sire-sam <sire-sam@users.noreply.github.com>
2022-12-04 21:34:58 +0100

    Day 3 - part 2

--- days/3/index.ts
+++ days/3/index.ts
@@ -1,41 +1,78 @@
-import { RegExp } from "https://cdn.esm.sh/v78/webidl-conversions@3.0.1/es2021/webidl-conversions.js";
-
 const textDecoder = new TextDecoder("utf-8");
 const dirPath = new URL(".", import.meta.url).pathname;
 
 const isRunningTest = false;
 const testInputPath = dirPath + "./test.txt";
 const inputPath = dirPath + "./input.txt";
-
+const contentOf = (filePath: string) => Deno.readFile(filePath).then((res) => textDecoder.decode(res));
 const chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
-const expectedTestResult = 157;
+
+const part1TestExpected = 157;
 
 const splitLineHalf = (l: string): [string, string] => [l.slice(0, l.length / 2), l.slice(l.length / 2, l.length)];
 const charMatchInLines = ([l1, l2]: [string, string]) => l2[l2.search(new RegExp(`[${l1}]`))];
 const charWeight = (char: string) => chars.indexOf(char) + 1;
 const sum = (a, b) => a + b;
-const logEntry = (e) => console.log(e);
+const logEntry = (e) => {
+  console.log(e);
+  return e;
+};
 
-Deno.readFile(isRunningTest ? testInputPath : inputPath)
+// Part One
+// --------
+contentOf(isRunningTest ? testInputPath : inputPath).then(
+  (res) => res
+    .split(/\n/)
+    .map(splitLineHalf)
+    .map(charMatchInLines)
+    .map(charWeight)
+    .reduce(sum, 0)
+).then((r) => {
+    if (isRunningTest) {
+      console.assert(r === part1TestExpected, r, `should be ${part1TestExpected}`);
+    }
+    console.log("Part 1 answer", r);
+  }
+);
+
+// Part two
+// --------
+const testPart2Path = dirPath + "./test-part-2.txt";
+const part2TestExpected = 70;
+
+function groupByThree(acc: [string?, string?, string?][], entry: string): [string?, string?, string?][] {
+  acc.length && acc[acc.length - 1].length < 3
+    ? acc[acc.length - 1].push(entry)
+    : acc.push([entry]);
+  return acc;
+};
+
+function charMatchIn([l1, l2, l3]: [string, string, string]) {
+  const l1Chars = new RegExp(`[${l1}]`, "g");
+  const matchL2 = new Set(l2.match(l1Chars));
+  const matchL3 = new Set(l3.match(l1Chars));
+  const l2Entries = matchL2.entries();
+  let entryValue;
+
+  while ((entryValue = l2Entries.next().value[0]) !== undefined) {
+    if (matchL3.has(entryValue)) {
+      return entryValue;
+    }
+  }
+}
+
+contentOf(isRunningTest ? testPart2Path : inputPath)
   .then(
-    (res) => textDecoder.decode(res)
+    (r) => r
       .split(/\n/)
-      .map(splitLineHalf)
-      .map(charMatchInLines)
+      .reduce(groupByThree, [])
+      .map(charMatchIn)
       .map(charWeight)
       .reduce(sum, 0)
-  )
-  .then((r) => {
-      if (isRunningTest) {
-        console.assert(r === expectedTestResult, r, `should be ${expectedTestResult}`);
-      }
-      console.log(r);
+  ).then((r) => {
+    if (isRunningTest) {
+      console.assert(r === part2TestExpected, r, `should be ${part2TestExpected}`);
     }
-  );
-
-// Split by line
-//   for each
-//      split Hald (length/2) ?
-//      find letter in both string
-//      get weight of letter
-//      sum + weight
+    console.log("Part 2 answer", r);
+  }
+);
